f7d5995410723fc1a26c212eb303ac1836512048
Milagros Alfonzo <58148530+milokett@users.noreply.github.com>
2020-03-23 18:07:51 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,12 @@
+// funciones goToGame realizan el cambio de pantalla según el personaje elegido, haciendo visible o no ciertos elementos.
+
 function goToGame1(){
     document.getElementById("lamponne").style.visibility = "visible";
     document.getElementById("santoss").style.display = "none";
     document.getElementById("ravenna").style.display = "none";
     document.getElementById("medina").style.display = "none";
     document.getElementById("pj").style.display = "none";
-    document.getElementById("title").innerHTML = "<h2> Comencemos con el operativo </h2>";
+    document.getElementById("title").innerHTML = "<h2> Lamponne, comencemos con el operativo. </h2>";
 }
 
 function goToGame2(){
@@ -13,7 +15,7 @@ function goToGame2(){
     document.getElementById("ravenna").style.display = "none";
     document.getElementById("medina").style.display = "none";
     document.getElementById("pj").style.display = "none";
-    document.getElementById("title").innerHTML = "<h2> Comencemos con el operativo </h2>";
+    document.getElementById("title").innerHTML = "<h2> Santos, comencemos con el operativo. </h2>";
 }
 
 function goToGame3(){
@@ -22,7 +24,7 @@ function goToGame3(){
     document.getElementById("santoss").style.display = "none";
     document.getElementById("medina").style.display = "none";
     document.getElementById("pj").style.display = "none";
-    document.getElementById("title").innerHTML = "<h2> Comencemos con el operativo </h2>";
+    document.getElementById("title").innerHTML = "<h2> Camaleon, comencemos con el operativo. </h2>";
 }
 
 function goToGame4(){
@@ -31,8 +33,10 @@ function goToGame4(){
     document.getElementById("santoss").style.display = "none";
     document.getElementById("ravenna").style.display = "none";
     document.getElementById("pj").style.display = "none";
-    document.getElementById("title").innerHTML = "<h2> Comencemos con el operativo </h2>";
+    document.getElementById("title").innerHTML = "<h2> Medina, comencemos con el operativo. </h2>";
 }
+
+// La función option realiza la logica del juego basandose en una tabla que decidira si se gana, pierde o empata
 
 let click = 0;
 
@@ -42,8 +46,6 @@ function option(user){
     let textResult = ["Archivaremos su caso y seguramente en un futuro lo vamos a ayudar. Pero de algo puede estar seguro: su problema, para nosotros, no implica ningún tipo de prioridad.", "El caso fue completado con exito, pero una cosa más, usted quedaría comprometido con nosotros para algún otro operativo.", "El operativo se salió de control, y nuestros colaboradores quedaron detenidos por el FBI, resolveremos su caso tan pronto resolvamos este inconveniente."];
     
     let namePj = [ "Tendremos que tomar medidas drasticas", "Llamemos al primo de Satanas", "El plan marcha bien. Mientras tanto, preguntenle a Vanegas si tiene noticias del caso Milazzo ", "Al termino del episodio oral señores", "Llamó el contador y dijo que nos ibamos de presupuesto"];
-
-    let nameEnemy = ["", "", "", "", ""];
 
     let move = [
 
@@ -62,7 +64,8 @@ function option(user){
     let text = document.getElementById("result");
 
     text.innerHTML = `<h3> ${namePj[user]} </h3>`;
-                    
+    
+//La variable click comienza en cero y aumenta cada vez que se clickea para ejecutar la función, si se llega a tres muestra el resultado del juego
     if (click === 3){
         text.innerHTML = `
                         <h3> ${textResult[result]} </h3>`;
